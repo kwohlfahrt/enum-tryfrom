3ca1d850fd852a7648a1edb80ea78957574b45a4
Kai Wohlfahrt <kai.scorpio@gmail.com>
2017-05-28 19:44:18 +0100

    Add some benchmarks
    Test to show generated if/else tree is close to optimal (at least for small
    structs). About 4x slower than pure transmute.

---
+++ benches/main.rs
@@ -0,0 +1,44 @@
+#![feature(test)]
+
+#[macro_use]
+extern crate enum_derive;
+extern crate rand;
+extern crate test;
+
+use test::Bencher;
+use std::mem::transmute;
+use rand::Rng;
+
+#[derive(PartialEq,Eq,Debug,FromPrimitive)]
+#[FromPrimitiveType="u8"]
+enum Foo {
+    ZerothFoo = 0,
+    FirstFoo,
+    SecondFoo,
+    ThirdFoo,
+}
+
+#[bench]
+fn bench_raw(b: &mut Bencher) {
+    let mut values = (0..255).map(|x| x % 4).collect::<Vec<u8>>();
+    let mut rng = rand::thread_rng();
+    rng.shuffle(&mut values);
+
+    b.iter(|| {
+        values.iter().map(|x| {
+            if *x > 3 as u8 {panic!("x out of range!")};
+            unsafe{transmute::<u8, Foo>(*x)}
+        }).collect::<Vec<Foo>>()
+    })
+}
+
+#[bench]
+fn bench_from(b: &mut Bencher) {
+    let mut values = (0..255).map(|x| x % 4).collect::<Vec<u8>>();
+    let mut rng = rand::thread_rng();
+    rng.shuffle(&mut values);
+
+    b.iter(|| {
+        values.iter().map(|x| Foo::from(*x)).collect::<Vec<Foo>>()
+    })
+}
